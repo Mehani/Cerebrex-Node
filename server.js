7944153bd130e1c92a2fe176bf3b768a163d105b
Mehani <kmehani@yahoo.com>
2019-05-09 10:04:53 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express'),
+    path = require('path'),
+    bodyParser = require('body-parser'),
+    cors = require('cors'),
+    
+    const app = express();
+    app.use(bodyParser.json());
+    app.use(cors());
+    const port = process.env.PORT || 4000;
+
+    const server = app.listen(port, function(){
+     console.log('Listening on port ' + port);
+    });
